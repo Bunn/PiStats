3557efe97f750dd2480a28d6e5b14980c5e35c7b
Fernando Bunn <bunn@idevzilla.com>
2020-04-25 16:43:06 +0100

    Initial Commit

---
+++ PiHoleStats/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PiHoleStats
+//
+//  Created by Fernando Bunn on 25/04/2020.
+//  Copyright © 2020 Fernando Bunn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
