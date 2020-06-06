ac6d77577fb162afa3b78987d8fa374f6a4720e9
Fernando Bunn <bunn@idevzilla.com>
2020-06-06 14:54:01 +0100

    WIP: Handle pi-hole selection list

--- PiHoleStats/Views/Preferences/PiholeConfigView.swift
+++ PiHoleStats/Views/Preferences/PiholeConfigView.swift
@@ -11,53 +11,61 @@ import SwiftUI
 struct PiholeConfigView: View {
     @EnvironmentObject var piHoleController: PiholeController
     @State private var selectedItem: Pihole?
-    @State var items = [String]()
-
+    @State var items = [Pihole]()
+    
     var body: some View {
-            HStack {
-                VStack(alignment: .leading, spacing: 0) {
-                    List(selection: $selectedItem) {
-                        ForEach(piHoleController.piholes) { pihole in
-                            Text(pihole.address).tag(pihole)
-                        }
+        HStack {
+            VStack(alignment: .leading, spacing: 0) {
+                List(selection: $selectedItem) {
+                    ForEach(piHoleController.piholes) { pihole in
+                        Text(pihole.address).tag(pihole)
                     }
+                }
+                HStack(spacing: 0) {
+                    Button(action: {
+                        self.addStubPihole()
+                    }, label: {
+                        Text("Add")
+                    })
                     
-                    HStack(spacing: 0) {
-                        Button(action: {
-                            self.addStubPihole()
-                        }, label: {
-                            Text("Add")
-                        })
-                        
-                        Button(action: {
-                            self.removeSelectedPihole()
-                        }, label: {
-                            Text("Remove")
-                        }).disabled(selectedItem == nil)
+                    Button(action: {
+                        self.removeSelectedPihole()
+                    }, label: {
+                        Text("Remove")
+                    }).disabled(selectedItem == nil)
+                }
+            }
+            if selectedItem != nil {
+                PiholeItemConfigView(piholeViewModel: PiholeViewModel(piHole: selectedItem!))
+            } else {
+                Spacer()
+                VStack {
+                    if piHoleController.piholes.count > 0 {
+                        Text("Select a pi-hole on the left or click Add to setup a new pi-hole.")
+                            .multilineTextAlignment(.center)
+                    } else {
+                        Text("No pi-holes available, click Add to setup a new pi-hole")
+                            .multilineTextAlignment(.center)
                     }
-                }
-                if selectedItem != nil {
-                    PiholeItemConfigView(piholeViewModel: PiholeViewModel(piHole: selectedItem!))
-                } else {
-                    Text("Select an item")
-                    Spacer()
-
-                }
-            }.onAppear {
-                self.selectedItem = self.piHoleController.piholes.first
+                }.padding()
+                Spacer()
+            }
+        }.onAppear {
+            self.selectedItem = self.piHoleController.piholes.first
         }
     }
     
     private func addStubPihole() {
         let pihole = piHoleController.addStubPihole()
-        items.append(pihole.id.uuidString)
+        items.append(pihole)
+        selectedItem = pihole
     }
     
     private func removeSelectedPihole() {
         if let pihole = self.selectedItem {
             remove(pihole)
         }
-        self.selectedItem = nil
+        selectedItem = nil
     }
     
     private func remove(_ pihole: Pihole) {
