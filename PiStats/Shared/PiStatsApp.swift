8d818a3c3f215eb7509f0e185035d1b896c0ef6e
Fernando Bunn <bunn@idevzilla.com>
2021-06-05 10:42:13 +0100

    Initial refactor commit

---
+++ PiStats/Shared/PiStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiStatsApp.swift
+//  Shared
+//
+//  Created by Fernando Bunn on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct PiStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
