df66234ae484bac534e1981b3eade9378b000642
Fernando Bunn <bunn@idevzilla.com>
2024-05-08 20:50:52 +0100

    test code

--- PiStats/PiStats/macOS/MacTest.swift
+++ PiStats/PiStats/macOS/MacTest.swift
@@ -6,9 +6,21 @@
 //
 
 import Foundation
+import PiStatsCore
 
 struct MacTest {
     static func test() {
         print("BAtata")
+
+        let server = ServerSettings(version: .v5, host: "x", requestProtocol: .http)
+        let credentials = Credentials(apiToken: "x")
+        let pihole = Pihole(serverSettings: server, credentials: credentials)
+
+        let manager = PiholeManager(pihole: pihole)
+
+        Task {
+            try await manager.updateSummary()
+            print("P \(manager.pihole.summary)")
+        }
     }
 }
